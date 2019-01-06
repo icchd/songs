649ff512e6ca9ce80a66df2c2fcf4a33148555df
Savio Dimatteo <darksmo@gmail.com>
2019-01-06 10:41:35 +0100

    update project with correct dates

--- js/catholicHolidays.js
+++ js/catholicHolidays.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.catholicHolidays = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.catholicHolidays = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
@@ -117,8 +117,7 @@ function getPreviousFestiveDay(m, oStartFromDay) {
 function getFirstSundayOfAdvent(m, iYear) {
     // Nearest Sunday to 30 November
     var o30Nov = m({ year: iYear, month: 10, day: 30 });
-    var iWeekDay30Nov = o30Nov.weekday();
-    return o30Nov.add(7 - iWeekDay30Nov, "days");
+    return _getNextDay(o30Nov, "Sunday");
 }
 function getSecondSundayOfAdvent(m, iYear) {
     return getFirstSundayOfAdvent(m, iYear).add(1, "week");
@@ -165,6 +164,14 @@ function _getSuccessiveFestiveDay(m, oStartDay, aFestiveDaysThisYear, bForwardDi
     });
 
     return aNextFestiveDays[0];
+}
+
+function _getNextDay(oStartDay, sDayName) {
+    var oDay = oStartDay.clone();
+    while (oDay.format("dddd") !== sDayName) {
+        oDay.add(1, "day");
+    }
+    return oDay;
 }
 
 function _isFestiveDay(oDay, aFestiveDays) {
